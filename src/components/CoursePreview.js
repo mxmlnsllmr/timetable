7a5901021ecc152c2fee0d6b75936b0659fcd897
Maximilian Sellmer <maximiliansellmer@gmail.com>
2017-02-08 16:50:47 +0100

    fiexed code styling

--- src/components/CoursePreview.js
+++ src/components/CoursePreview.js
@@ -4,7 +4,11 @@ export default class CoursePreview extends React.Component {
   render() {
     return (
         <div className="course-preview list-group">
-          <h4 className="name list-group-item active course-preview-top test2">{this.props.name}<div className="test"><div className="glyphicon glyphicon-remove"></div></div></h4>
+          <h4 className="name list-group-item active course-preview-top test2">{this.props.name}
+            <div className="test">
+              <div className="glyphicon glyphicon-remove"></div>
+            </div>
+          </h4>
           <h4 className="list-group-item course-preview-items">{this.props.teacher}</h4>
           <h4 className="list-group-item course-preview-items">{this.props.place}</h4>
         </div>

76b46d048178cc8988f8ee84aee89d00dcee3b8c
Maximilian Sellmer <maximiliansellmer@gmail.com>
2017-02-09 08:17:57 +0100

    added functionality for deleting CoursePreview - just the function - not the functionality :D

--- src/components/CoursePreview.js
+++ src/components/CoursePreview.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 export default class CoursePreview extends React.Component {
+
+  deleteCourse(){
+    console.log('delete Course');
+  }
+
   render() {
     return (
         <div className="course-preview list-group">
           <h4 className="name list-group-item active course-preview-top test2">{this.props.name}
             <div className="test">
-              <div className="glyphicon glyphicon-remove"></div>
+              <div className="glyphicon glyphicon-remove" onClick={this.deleteCourse}></div>
             </div>
           </h4>
           <h4 className="list-group-item course-preview-items">{this.props.teacher}</h4>
